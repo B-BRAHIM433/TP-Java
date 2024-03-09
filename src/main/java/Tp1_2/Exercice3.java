41102bdd545a3678cf51e534f3c36572e20817d7
B-BRAHIM433 <brahimbaychou83@gmail.com>
2024-03-09 20:31:59 +0100

    commiting tp

--- src/main/java/Tp1_2/Exercice3.java
+++ src/main/java/Tp1_2/Exercice3.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Tp1_2;
+
+import java.util.Scanner;
 
 /**
  *
  * @author hp
  */
 public class Exercice3 {
-    
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Entrez un entier positif supérieur à 1 : ");
+        int n = scanner.nextInt();
+        int somme = 0;
+        if (n <= 1) {
+            System.out.println(n + " n'est pas un nombre parfait.");
+        } else {
+            for (int i = 1; i < n; i++) {
+                if (n % i == 0) {
+                    somme += i;
+                }
+            }
+            if (somme == n) {
+                System.out.println(n + " est un nombre parfait.");
+            } else {
+                System.out.println(n + " n'est pas un nombre parfait.");
+            }
+        }
+    }
 }
